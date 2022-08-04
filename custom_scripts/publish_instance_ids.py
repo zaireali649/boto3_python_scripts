5bc652754f9a505220b78f3bd27de598176cd416
Zaire Ali <zaireali649@gmail.com>
2022-08-04 23:41:05 +0000

    import publish_sns_topic

--- custom_scripts/publish_instance_ids.py
+++ custom_scripts/publish_instance_ids.py
@@ -5,6 +5,11 @@ import boto3
 sys.path.append('../')
 
 import ec2.list_instance_ids as lii
+import sns.publish_sns_topic as pst
 
 ec2 = boto3.client('ec2')
-lii.list_instance_ids(ec2)
+instanceIds = lii.list_instance_ids(ec2)
+print(instanceIds)
+
+sns = boto3.client('sns')
+pst.sns_publish_topic(sns, 'arn:aws:sns:us-east-1:458806987020:Idontknow', 'JSONhariston did it!!!!')
