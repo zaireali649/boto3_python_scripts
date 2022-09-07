503db7f3bcc7cd85442ed6ca7cc2d91b0a8524a3
Zaire Ali <ZAIREALI649@gmail.com>
2022-09-07 00:55:16 +0000

    upload_object

--- s3/upload_object.py
+++ s3/upload_object.py
@@ -3,7 +3,8 @@ import boto3
 s3 = boto3.client('s3')
 
 filename = 'upload_object.py'
+bucket_name="zali-catch-all"
 
 with open(filename, 'rb') as data:
-    s3.upload_fileobj(data, 'zali-catch-all', filename)
+    s3.upload_fileobj(data, bucket_name, filename)
     print("Object Uploaded")
