3ea5e43d7d31f15726d90360ad95667f844f9510
Zaire Ali <ZAIREALI649@gmail.com>
2022-09-09 00:27:30 +0000

    create_message

--- sqs/create_message.py
+++ sqs/create_message.py
@@ -3,7 +3,7 @@ import boto3
 sqs = boto3.client('sqs')
 
 response = sqs.send_message(
-    QueueUrl='https://sqs.us-east-1.amazonaws.com/458806987020/gold-queue',
+    QueueUrl='https://queue.amazonaws.com/458806987020/gold-queue',
     MessageBody='Message sent from Python <3'
 )
 
