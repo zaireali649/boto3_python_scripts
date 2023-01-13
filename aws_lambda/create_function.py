3370c578e2a3f6795d18e1019342105683d96bba
Zaire Ali <zaireali649@gmail.com>
2023-01-13 00:31:58 +0000

    create_function

--- aws_lambda/create_function.py
+++ aws_lambda/create_function.py
@@ -1,16 +1,24 @@
 import boto3
 
-aws_lambda = boto3.client('lambda')
+def create_function(client, roleARN, s3_bucket, s3_key, functionName='python_boto3_lambda', runtime='python3.8', handler='lambda_function.lambda_handler'):
+    response = client.create_function(
+        FunctionName=functionName,
+        Runtime=runtime,
+        Handler=handler,
+        Role=roleARN,
+        Code={
+            'S3Bucket': s3_bucket,
+            'S3Key': s3_key,
+        }
+    )
+    
+    return response
 
-response = aws_lambda.create_function(
-    FunctionName='black_boto3_lambda',
-    Runtime='python3.8',
-    Handler='lambda_function.lambda_handler',
-    Role='arn:aws:iam::458806987020:role/service-role/luit_red_jan_2023_lambda-role-fo8jlfny',
-    Code={
-        'S3Bucket': 'zali-catch-all',
-        'S3Key': 'lambda_function.zip',
-    }
-)
 
-print(response)
+
+if(__name__ == "__main__"):
+    aws_lambda = boto3.client('lambda')
+    
+    response = create_function(aws_lambda, 'arn:aws:iam::458806987020:role/service-role/luit_red_jan_2023_lambda-role-fo8jlfny', 'zali-catch-all', 'lambda_function.zip')
+    
+    print(response)
