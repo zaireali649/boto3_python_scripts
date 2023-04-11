354ee88b86f0cc160dcb417749deb6a3512df449
Stacey Montes <getatmexpress@gmail.com>
2023-04-11 20:58:56 +0000

    Made_Edits

--- s3/creation_Date.py
+++ s3/creation_Date.py
@@ -1,12 +1,5 @@
 import boto3
 
-#s3_client = boto3.client('s3')
-#bucket_name = s3_client.Bucket('bucket-name')
-#creation_date = s3_client.meta.client.head_bucket(Bucket=bucket_name)['ResponseMetadata']['HTTPHeaders']['date']
-#print("Bucket creation date:", creation_date)
-
-s3_client = boto3.client('s3')
-bucket_name = 'my-bucket-name'
-response = s3_client.head_bucket(Bucket=bucket_name)
-creation_date = response['CreationDate']
+#s3_client = boto3.client('s3')#bucket_name = s3_client.Bucket('bucket-name')
+creation_date = s3_client.meta.client.head_bucket(Bucket=bucket_name)['ResponseMetadata']['HTTPHeaders']['date']
 print("Bucket creation date:", creation_date)
--- s3/upload_object.py
+++ s3/upload_object.py
@@ -5,11 +5,12 @@ import glob
 
 s3 = boto3.client('s3')
 
-filename = 'list_object.py'
+filename = 'list_buckets.py'
 bucket_name="smontes-catch-all"
 
 with open(filename, 'rb') as data:
     s3.upload_fileobj(data, bucket_name, filename)
     print("Object Uploaded")
     
-cwd=os.getcwd()
+#cwd=os.getcwd()
+#cwd=cwd+"/upload"
