ce762d56e348dd68470d83b2aa8ef1285103693c
Katie Sheridan <johnsonkatie71@yahoo.com>
2023-04-11 23:13:40 +0000

    variables_and_datatypes-04112023

---
+++ AWS-Variables.py
@@ -0,0 +1,13 @@
+some_string = "Hello Python"
+print(some_string)
+
+#Variable Examples
+a_str = "This is an example of a string in quotes" # In python the word string is abbreviated to str
+my_float = 5.5
+an_integer = 5 # integer is abbreviated to int
+shopping_list = ["apples", "oranges", "pears"]
+a_dict = {"userId": "JBloggs"} # dictionary is abbreviated to dict
+my_var = another_variable # variable is abbreviated to var
+test_function = my_function() #function is abbreviated to func
+example_tuple = ("apple", "orange", "pear")
+boolean_values = True # boolean is abbreviated to bool
