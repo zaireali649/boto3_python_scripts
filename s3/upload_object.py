b2c0ef4702e9ef1b9b427f6ded3e935c5c2fb4a2
Stacey <124016788+StaceyM0@users.noreply.github.com>
2023-04-10 20:03:01 -0600

    Update upload_object.py

--- s3/upload_object.py
+++ s3/upload_object.py
@@ -3,7 +3,7 @@ import boto3
 s3 = boto3.client('s3')
 
 filename = 'upload_object.py'
-bucket_name="zali-catch-all"
+bucket_name="smontes-catch-all"
 
 with open(filename, 'rb') as data:
     s3.upload_fileobj(data, bucket_name, filename)
