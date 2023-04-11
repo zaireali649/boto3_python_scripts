0657835643b455df8aa4f01c4e793390d11c8a62
Stacey Montes <getatmexpress@gmail.com>
2023-04-11 02:56:32 +0000

    creation_date

---
+++ s3/creation_Date.py
@@ -0,0 +1,10 @@
+import boto3
+s3_resource=boto3.client
+s3_resource=boto3.client("s3")
+
+s3_resource.list_buckets()["Buckets"][0]["Name"]
+creation_date=s3_resource.list_buckets()["Buckets"][0]["CreationDate"]
+creation_date.strftime("%d%m%y_%H:%M:%s")
+
+print(creation_date)
+print( Buckets["Name"])
