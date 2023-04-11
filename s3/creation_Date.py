dd480ecc52db67ccd9fbb9c7aa127e18c9f05c0f
Stacey Montes <getatmexpress@gmail.com>
2023-04-11 20:34:31 +0000

    Added_code

--- s3/creation_Date.py
+++ s3/creation_Date.py
@@ -1,10 +1,12 @@
 import boto3
-s3_resource=boto3.client
-s3_resource=boto3.client("s3")
 
-s3_resource.list_buckets()["Buckets"][0]["Name"]
-creation_date=s3_resource.list_buckets()["Buckets"][0]["CreationDate"]
-creation_date.strftime("%d%m%y_%H:%M:%s")
+#s3_client = boto3.client('s3')
+#bucket_name = s3_client.Bucket('bucket-name')
+#creation_date = s3_client.meta.client.head_bucket(Bucket=bucket_name)['ResponseMetadata']['HTTPHeaders']['date']
+#print("Bucket creation date:", creation_date)
 
-print(creation_date)
-print( Buckets["Name"])
+s3_client = boto3.client('s3')
+bucket_name = 'my-bucket-name'
+response = s3_client.head_bucket(Bucket=bucket_name)
+creation_date = response['CreationDate']
+print("Bucket creation date:", creation_date)
