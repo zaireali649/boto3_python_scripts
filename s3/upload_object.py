c85a8d10fa4bf433b16270cb31a8447054f0c31b
Stacey Montes <getatmexpress@gmail.com>
2023-04-11 21:33:10 +0000

    Added_multiple_files

--- s3/upload_object.py
+++ s3/upload_object.py
@@ -11,7 +11,15 @@ bucket_name="smontes-catch-all"
 with open(filename, 'rb') as data:
     s3.upload_fileobj(data, bucket_name, filename)
     print("Object Uploaded")
-    
+  
+#how to upload multiple files  
 #cwd=os.getcwd()
 #cwd=cwd+"/upload"
-
+#files=glob.glob(cwd+"*.png") #all png files
+#files
+#for file in files:
+ #   s3 =boto3.client('s3')
+ #   s3.upload_fileobj(
+ #      filename=file,
+ #      bucket_name="smontes-catch-all",
+ #      Key=file.split("/")[-1])
