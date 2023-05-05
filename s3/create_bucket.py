09a1986e4fbf087127cad3473a3c996a1b8a69b7
Dijidabubu <alexisjchavez@gmail.com>
2023-05-05 11:50:41 -0400

    created bucket for shoppingcartapp

--- s3/create_bucket.py
+++ s3/create_bucket.py
@@ -1,11 +1,9 @@
-#%%
+#!/bin/bash
 
-# create a S3 Bucket
+#create a S3 Bucket
 
 import boto3
 
 client = boto3.client("s3")
 
-#%%
-
-client.create_bucket(Bucket="zali-bucket-test-boto3") 
+client.create_bucket(Bucket="acha-boto3-shoppingcartapp")
