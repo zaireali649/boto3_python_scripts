556a762617f35076763e29994926e53414eb6afd
Zaire Ali <zaireali649@gmail.com>
2023-10-16 23:54:14 +0000

    create bucket mod

--- s3/create_bucket.py
+++ s3/create_bucket.py
@@ -3,9 +3,12 @@
 # create a S3 Bucket
 
 import boto3
+import json
 
 client = boto3.client("s3")
 
 #%%
 
-client.create_bucket(Bucket="zali-bucket-test-boto3") 
+response = client.create_bucket(Bucket="zali-bucket-test-boto3") 
+
+print(json.dumps(response, indent=4))
